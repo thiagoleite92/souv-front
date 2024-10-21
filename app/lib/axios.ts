1f2489a24590828117287f9b7f8b427dbf85ce89
Thiago Leite <leite.tjs@gmail.com>
2024-10-20 23:52:29 -0300

    feat: add api url

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5555/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL
+    ? process.env.NEXT_PUBLIC_API_URL
+    : 'http://localhost:5555/api',
 });
